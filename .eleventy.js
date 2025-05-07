4ddddac2a0aa448e97450c39798416941c514a7f
AlamiSadHomie <62135669+AlamiSadHomie@users.noreply.github.com>
2025-05-07 12:33:14 +0800

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -23,7 +23,7 @@ module.exports = function(eleventyConfig) {
   return {
     dir: {
       input: "src",
-      output: "joyful-rabanadas-e4f399",
+      output: "public",
       includes: "_includes",
       data: "_data"
     },
