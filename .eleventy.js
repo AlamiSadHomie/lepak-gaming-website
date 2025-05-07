3994a551ce97bb91a28c6d69a45fe3c237c6b915
AlamiSadHomie <62135669+AlamiSadHomie@users.noreply.github.com>
2025-05-07 12:22:39 +0800

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -23,7 +23,7 @@ module.exports = function(eleventyConfig) {
   return {
     dir: {
       input: "src",
-      output: "public",
+      output: "https://joyful-rabanadas-e4f399.netlify.app/",
       includes: "_includes",
       data: "_data"
     },
